1759f58032036ca2b8cd73f2d33008a3a01ad768
d4rk3n <aravindmk@outlook.com>
2020-09-20 12:12:14 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -8,19 +8,19 @@ class queue:
     def dequeue(self):
         return self.items.pop(0)
     
-s=queue()
+q=queue()
 while True:
     print("\n1 Put\n2 Get\n3 Exit\n")
     ch=int(input("Enter your choice : "))
     if ch==1:
         value=int(input("Enter the item : "))
-        s.enqueue(value)
+        q.enqueue(value)
         print("\nSuccess :)")
     elif ch==2:
-        if s.is_empty():
+        if q.is_empty():
             print("Underflow")
         else:
-            print("\nPopped item : ",s.dequeue())
+            print("\nPopped item : ",q.dequeue())
     elif ch==4:
         exit()
     else:
